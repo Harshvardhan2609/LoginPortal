e4c978fa7a471188dc548193f43a78c2ce071998
Harshvardhan2609 <harshvardhansingh2609@gmail.com>
2023-08-22 02:01:31 +0530

    Items alligned to center

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import strings from "../Constants/StringConstants";
+
 const Login = () => {
-    return (
+  return (
+    <div>
+      <div style={styles.headerTextDiv}>
+        <h1 style={styles.welcomeTextStyles}>Welcome</h1>
+
         <div>
-            <h3>{strings.loginPageText}</h3>
-            <Link to="/home" >{strings.goToText} Home</Link><br/>
-            <Link to="/SignUp" >{strings.goToText} SignUp</Link><br/>
-            <Link to="/ForgotPassword" >{strings.goToText} ForgotPassword</Link>
+          <input type placeholder="Username" />
+          <br />
+          <input type placeholder="Password" />
+          <br />
         </div>
-    );
+        <div>
+          <button style={styles.buttonTextStyles}>LOGIN</button>
+        </div>
+        {/* <Link to="/home">{strings.goToText} Home</Link> */}
+        {/* <br /> */}
+        <Link to="/SignUp">{strings.goToText} SignUp</Link>
+        <br />
+        <Link to="/ForgotPassword">{strings.goToText} ForgotPassword</Link>
+      </div>
+    </div>
+  );
 };
+const styles = {
+  welcomeTextStyles: {
+    textAlign: "center",
+    fontWeight: "900",
+  },
+  headerTextDiv: {
+    marginTop: "100px",
+    textAlign: "center",
+  },
+  buttonTextStyles: {
+    borderRadius: "50px",
+    width: "150px",
+    height: "30px",
+    marginTop: "10px",
+    backgroundColor: "Green",
+    textAlign: "center",
+  },
+};
+
 export default Login;
