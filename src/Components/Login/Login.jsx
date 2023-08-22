c90db02d7de037770d6e560000ecefb8b9b5f578
Harshvardhan2609 <harshvardhansingh2609@gmail.com>
2023-08-22 17:21:23 +0530

    Completed login api integration module

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import strings from "../Constants/StringConstants";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [userName, setuserName] = useState("");
+  const [password, setpassword] = useState("");
+
+  const onPressConfirmPasswordButton = () => {
+    let LoginObjectToBeSentToAPI = {
+      userName: userName,
+      password: password,
+    };
+    alert("Data saved successfully");
+  };
+
+  const onChangeUserName = (text) => {
+    setuserName(text);
+    console.log("handle change called" + userName);
+  };
+
+  const onChangepassword = (text) => {
+    setpassword(text);
+    console.log("handle change called" + password);
+  };
+
   //   const [dummyApiData, setDummyApiData] = useState({});
   //   const dummyApiCall = () => {
   //     fetch("https://reqres.in/api/users?page=2")
@@ -22,23 +45,44 @@ const Login = () => {
 
   //   useEffect(() => {
   //   }, [dummyApiData]);
-
+  const onPressLoginButton = () => {
+    // alert();
+    let LoginObjectToBeSentToAPI = {
+      userName: userName,
+      password: password,
+    };
+    alert("Data saved successfully");
+    console.log("Data is :" + JSON.stringify(LoginObjectToBeSentToAPI));
+    navigate("Home");
+  };
   return (
     <div className="Body">
       <div style={styles.headerTextDiv}>
         <h1 style={styles.welcomeTextStyles}>Welcome</h1>
-
         <div className="Input" style={styles.inputstyleText}>
-          <input type placeholder="Username" />
+          <input
+            type
+            placeholder="Username"
+            value={userName}
+            onChange={(obj) => onChangeUserName(obj.target.value)}
+          />
           <br />
-          <input type placeholder="Password" />
+          <input
+            type
+            placeholder="Password"
+            value={password}
+            onChange={(obj) => onChangepassword(obj.target.value)}
+          />
+
           <br />
         </div>
         <div className="Button">
-          <button style={styles.buttonTextStyles}>LOGIN</button>
+          <button onClick={onPressLoginButton} style={styles.buttonTextStyles}>
+            LOGIN
+          </button>
         </div>
-        {/* <Link to="/home">{strings.goToText} Home</Link> */}
-        {/* <br /> */}
+        {/* { <Link to="/home">{strings.goToText} Home</Link> */}
+        {<br />}
         <Link to="/SignUp">{strings.goToText} SignUp</Link>
         <br />
         <Link to="/ForgotPassword">{strings.goToText} ForgotPassword</Link>
